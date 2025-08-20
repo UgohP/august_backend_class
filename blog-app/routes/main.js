e1617381fb3dad523f1ab2d0166937489b97ce5d
Paschal Ugoh <paskyugoh@gmail.com>
2025-08-20 15:48:13 +0100

    finished today class

--- blog-app/routes/main.js
+++ blog-app/routes/main.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const Blog = require("../models/Blog");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "Blog App",
+      description: "A blog app created with Nodejs, Express and MongoDB",
+    };
+    const data = await Blog.find().sort({createdAt: -1});
+    res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
-  res.render("about");
+  const locals = {
+    title: "Blog App",
+    description: "about",
+  };
+  res.render("about", { locals });
 });
 
 module.exports = router;
