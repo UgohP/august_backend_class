1b4df0fb08047fafc3bded41f5377c33c9a47552
Paschal Ugoh <paskyugoh@gmail.com>
2025-08-25 15:29:41 +0100

    finished today class

--- blog-app/routes/main.js
+++ blog-app/routes/main.js
@@ -2,14 +2,53 @@ const express = require("express");
 const Blog = require("../models/Blog");
 const router = express.Router();
 
+/**
+ * GET request
+ * API endpoint to get all blogs
+ */
 router.get("/", async (req, res) => {
   try {
     const locals = {
       title: "Blog App",
       description: "A blog app created with Nodejs, Express and MongoDB",
     };
-    const data = await Blog.find().sort({createdAt: -1});
-    res.render("index", { locals, data });
+
+    const perPage = 5;
+    const page = parseInt(req.query.page) || 1;
+
+    const data = await Blog.aggregate({ $sort: { createdAt: -1 } })
+      .skip(perPage * page - perPage)
+      .limit(5)
+      .exec();
+
+    const count = await Blog.countDocuments();
+    const nextPage = page + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      currentPage: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET request
+ * API endpoint to get a particular blog
+ */
+router.get("/blog/:id", async (req, res) => {
+  try {
+    const data = await Blog.findById(req.params.id);
+
+    const locals = {
+      title: data.title,
+      description: "A blog app created with Nodejs, Express and MongoDB",
+    };
+    res.render("blog", { data, locals });
   } catch (error) {
     console.log(error);
   }
