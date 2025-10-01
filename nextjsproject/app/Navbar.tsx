43df037136c7836988e61dcb270404590fa3654e
Paschal Ugoh <paskyugoh@gmail.com>
2025-10-01 13:40:37 +0100

    done with the database setup

--- nextjsproject/app/Navbar.tsx
+++ nextjsproject/app/Navbar.tsx
@@ -1,8 +1,33 @@
 "use client";
+import classnames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issue", href: "/issues" },
+  ];
   return (
     <>
-      <button onClick={() => console.log("Hello World")}>Navbar</button>
+      <nav className="flex space-x-10 border-b mb-5 px-5 h-14 items-center">
+        <Link href="/">Logo</Link>
+        <ul className="flex space-x-10">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={classnames({
+                "text-gray-200 font-bold": link.href == currentPath,
+                "hover:text-gray-400 hover:font-bold transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </nav>
     </>
   );
 };
--- nextjsproject/app/issues/page.tsx
+++ nextjsproject/app/issues/page.tsx
@@ -1,5 +1,5 @@
 const page = () => {
-  return <div>About</div>;
+  return <div>Issues</div>;
 };
 
 export default page;
